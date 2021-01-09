b030223af3d6ea2d561f45cb2fb60aaf70de55eb
PAYET Guillaume <tiguillaume@orange.fr>
2021-01-09 15:46:35 +0100

    init

---
+++ index.html.php
@@ -0,0 +1,61 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="Assets/css/style.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+            crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/22b44f4659.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="navbar navbar-dark " style="background-color: #1976D2">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <i class="fas fa-laptop-code fa-2x"></i>
+                    <strong>Tutorat Web</strong>
+                </a>
+            </div>
+        </nav>
+    </div>
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4">
+            <h1 style="margin-left: 2vh; margin-top: 4vh;">Bienvenue sur le site de tutorat !</h1>
+        </div>
+        <div class="col-4"></div>
+    </div>
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4">
+            <div class="space">
+                <h2>Veuillez vous connecter ou vous inscrire :</h2>
+                <div class="row" style="margin-top: 5vh">
+                    <div class="col-3"></div>
+                    <div class="col-6">
+                        <button type="button" class="btn btn-outline-primary">Se connecter</button>
+                        <button type="button" class="btn btn-outline-success">S'inscrire</button>
+                    </div>
+                    <div class="col-3"></div>
+
+
+                </div>
+            </div>
+
+        </div>
+        <div class="col-4"></div>
+
+    </div>
+
+
+</div>
+</body>
+</html>
