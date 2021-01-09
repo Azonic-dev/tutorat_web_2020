72729055d93724a019ef541b381e8060141f71e2
PAYET Guillaume <tiguillaume@orange.fr>
2021-01-09 16:29:22 +0100

    ajout du header

--- index.html.php
+++ index.html.php
@@ -1,31 +1,9 @@
-<?php namespace App;?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Tutorat Web</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="Assets/css/style.css" rel="stylesheet">
+<?php namespace App;
+    require "script/header.php";
+?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-            crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/22b44f4659.js" crossorigin="anonymous"></script>
-
-</head>
 <body>
 <div class="container-fluid">
-    <div class="row">
-        <nav class="navbar navbar-dark " style="background-color: #1976D2">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <i class="fas fa-laptop-code fa-2x"></i>
-                    <strong>Tutorat Web</strong>
-                </a>
-            </div>
-        </nav>
-    </div>
     <div class="row">
         <div class="col-4"></div>
         <div class="col-4">
@@ -41,8 +19,8 @@
                 <div class="row" style="margin-top: 5vh">
                     <div class="col-3"></div>
                     <div class="col-6">
-                        <button type="button" class="btn btn-outline-primary">Se connecter</button>
-                        <button type="button" class="btn btn-outline-success">S'inscrire</button>
+                        <a href="page_connexion.html.php"><button type="button" class="btn btn-outline-primary">Se connecter</button></a>
+                        <a href="page_inscription.html.php"><button type="button" class="btn btn-outline-success">S'inscrire</button></a>
                     </div>
                     <div class="col-3"></div>
 
---
+++ script/header.php
@@ -0,0 +1,29 @@
+<?php
+$base = new PDO('mysql:host=localhost;dbname=tutorat_bdd', 'root', 'root');
+$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tutorat Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="Assets/css/style.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+            crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/22b44f4659.js" crossorigin="anonymous"></script>
+
+</head>
+<div class="row">
+    <nav class="navbar navbar-dark " style="background-color: #1976D2">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <i class="fas fa-laptop-code fa-2x"></i>
+                <strong>Tutorat Web</strong>
+            </a>
+        </div>
+    </nav>
+</div>
