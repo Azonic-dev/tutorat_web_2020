92c38eaabdb58d9b7416c5ccf5b762433881b600
PAYET Guillaume <tiguillaume@orange.fr>
2021-01-10 21:01:34 +0100

    Update connexion.php

--- Script/connexion.php
+++ Script/connexion.php
@@ -3,6 +3,6 @@ if (isset($_POST["connexionInputPseudo"]) && isset($_POST["connexionInputMotDePa
     $pseudo = $_POST["connexionInputPseudo"];
     $motdepasse = $_POST["connexionInputMotDePasse"];
 
-    //On mettra le code pour réaliser l'inscription ici
+    //On mettra le code pour réaliser la connexion ici
     echo "pseudo = ".$pseudo.", motdepasse = ".$motdepasse;
 }
